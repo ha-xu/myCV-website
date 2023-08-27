0f5139b200299bb3c2224da50c08a448dce6b348
COOKIE\11145 <xuzihansasc@163.com>
2023-08-27 22:19:06 +0200

    Code verification done / Add animation

--- jsFiles/index.js
+++ jsFiles/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    //for animation
+    //for animation of the title
     var gradientTitle = $("#PWEnterTitle h1");
     var titleStyle = window.getComputedStyle(gradientTitle[0]);
     var background = titleStyle.background;
@@ -29,4 +29,33 @@ $(document).ready(function() {
       }
     }
 
+    //event after the "Enter" key
+    $("#PWEnterBox input").on("keydown", function(event) {
+      if (event.key === "Enter") {
+          submitCode();
+      }
   });
+
+  });
+
+  function submitCode(){
+    var enteredText = $("#PWEnterBox input").val();
+    var encryptedCode = CryptoJS.SHA3(enteredText);
+    
+    if (encryptedCode.toString() == "98e2bdf3d45828f1d0d90e31157a613b6d1b27b52955f3d394b612fd1b29c88408bc2bb2cbb8eebd300484a2a791028ebb57fdcca4d7fd2449b4e139078b244e"){
+      $("#PWEnterBox a img").attr("src", "images/doneIcon.png");
+      $("#PWEnterBox a").css("background-color","rgb(75 189 103)");
+      setTimeout(function() {
+        window.location.href = "homePage.html";
+    }, 1000);
+    }else{
+      $("#PWEnterBox a img").attr("src", "images/xIcon.png");
+      $("#PWEnterBox a").css("background-color","rgb(240 81 53)");
+      setTimeout(function() {
+        $("#PWEnterBox a img").attr("src", "images/ArrowRightIcon.png");
+        $("#PWEnterBox a").css("background-color","rgb(235, 235, 235)");
+      }, 800);
+    
+    }
+    //  $("#PWEnterBox input").val(encryptedCode);
+  }
