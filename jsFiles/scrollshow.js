891f82d5a210df450cba27f8611c071bd06e89eb
COOKIE\11145 <xuzihansasc@163.com>
2023-09-30 01:38:17 +0200

    Add photos to experience page. Start about page

---
+++ jsFiles/scrollshow.js
@@ -0,0 +1,40 @@
+//need jquery
+
+const scrollshowObjs = document.querySelectorAll('.dynamicShow');
+
+    function handleScroll() {
+
+        var showPos = window.innerHeight / 5 * 4;
+        var hidePos = window.innerHeight / 18;
+
+        scrollshowObjs.forEach(obj => {
+            const objPosition = (obj.getBoundingClientRect().top + obj.getBoundingClientRect().bottom) / 2;
+
+            if (objPosition < showPos) {
+                if (objPosition < hidePos) {
+                    obj.classList.add(obj.id + '-hide');
+                    obj.classList.remove(obj.id + '-show');
+
+                } else {
+                    obj.classList.add(obj.id + '-show');
+                    obj.classList.remove(obj.id + '-hide');
+                }
+            } else {
+                obj.classList.add(obj.id + '-hide');
+                obj.classList.remove(obj.id + '-show');
+            }
+
+
+        });
+
+        if (window.location.hash) {
+            history.replaceState({}, document.title, window.location.pathname);
+        }
+
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    $(document).ready(function () {
+        handleScroll();
+    });
